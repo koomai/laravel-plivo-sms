7b7dd4b546a04d2f0f4de0ec64bbbb14a3f513f3
Sid <sid@koomai.net>
2016-08-10 17:25:47 +1000

    Added docblocks

--- src/Plivo.php
+++ src/Plivo.php
@@ -6,12 +6,28 @@ use Plivo\RestAPI as PlivoRestApi;
 
 class Plivo extends PlivoRestApi
 {
+
+    /**
+     * @var string
+     */
     protected $auth_id;
 
+    /**
+     * @var string
+     */
     protected $auth_token;
 
+    /**
+     * @var string
+     */
     protected $from;
 
+    /**
+     * Create a new Plivo RestAPI instance
+     *
+     * @param array $config
+     * @return void
+     */
     public function __construct(array $config)
     {
         $this->auth_id = $config['auth_id'];
@@ -21,6 +37,11 @@ class Plivo extends PlivoRestApi
         parent::__construct($this->auth_id, $this->auth_token);
     }
 
+    /**
+     * Number SMS is being sent from
+     *
+     * @return string
+     */
     public function from()
     {
         return $this->from;
